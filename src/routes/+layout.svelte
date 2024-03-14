d676ec2bb5fd1927d698e9f2295a38cc76d5fbbe
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-14 04:35:41 -0300

    header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
     import '../global.css'
     import '@fontsource-variable/roboto-condensed';
     import '@fontsource-variable/cabin';
+	import Header from '$lib/Header.svelte';
 </script>
 
+<Header />
 <slot />
