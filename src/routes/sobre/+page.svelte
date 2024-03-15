10788763562398a365523c277d394412f8413e7e
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-15 17:04:51 -0300

    funcionalidade: componente para perguntas e respostas

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -22,11 +22,32 @@
             the 1500s, when an unknown printer took a galley of type and scrambled it to make 
             a type specimen book."
         />
+
+        <Info 
+            question="Quanto custa a mensalidade ?"
+            answer="Lorem Ipsum is simply dummy text of the printing and typesetting 
+            industry. Lorem Ipsum has been the industry's standard dummy text ever since 
+            the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+            a type specimen book."
+        />
+
+        <Info 
+            question="Quanto custa a mensalidade ?"
+            answer="Lorem Ipsum is simply dummy text of the printing and typesetting 
+            industry. Lorem Ipsum has been the industry's standard dummy text ever since 
+            the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+            a type specimen book."
+        />
     </div>
 </section>
 
 <style>
     .faq {
         margin-top: 60px;
+    }
+    .faq__wrapper {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 20px;
     }
 </style>
