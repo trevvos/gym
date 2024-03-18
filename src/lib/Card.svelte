2d8c5093649f9bab36848bab3b0be8732195ec16
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-18 02:17:21 -0300

    single

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,11 +1,13 @@
 <script>
 // @ts-nocheck
-
     export let info;
+    import {page} from '$app/stores'
+
+    let archive = $page.route.id
 
 </script>
 
-<a href="/{info.slug}" class="card">
+<a href="{archive}/{info.slug}" class="card">
     <img src="{info.img}" alt="" width="120" height="120">
     <h2>{info.name}</h2>
 </a>
---
+++ src/routes/modalidades/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+// @ts-nocheck
+
+    import { page } from '$app/stores'
+    import { modalidades } from '$lib/stores'
+
+    let modalidade = $page.params.slug;
+
+    let data = $modalidades.find(element => element.slug === modalidade)
+</script>
+
+<h1>{data.name}</h1>
