69102f77aff04fe9a51191f6d02b83e500d827a4
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-19 00:49:43 -0300

    open graph meta tags

--- src/lib/SeoHome.svelte
+++ src/lib/SeoHome.svelte
@@ -1,4 +1,5 @@
 <script>
+    import img from '$lib/assets/musculacao.avif'
     export let description;
 
     let siteName = 'Academia Local'
@@ -8,4 +9,8 @@
 <svelte:head>
     <title>{siteName}{separator}A Academia pertinho de você!</title>
     <meta name="description" content="{description}">
+    <meta property="og:title" content="A Academia pertinho de você!">
+    <meta property="og:description" content="{description}" />
+    <meta property="og:image" content="{img}" />
 </svelte:head>
+
